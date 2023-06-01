ff4633707fed026b27a6548aa66697163867cd18
Chinedu <chinedu@beyondit.it>
2023-06-01 16:19:55 -0400

    Handled invalid value error

--- Convert.kt
+++ Convert.kt
@@ -12,36 +12,40 @@ fun main() {
     val oZTOG = 28.35
 
     println("Enter measurement unit (km, mi, cm, inch, lib, kg, ox, gram)")
-    val measurementUnit = readLine()
-    val formattedMeasurementUnit = measurementUnit
-    println("Enter value to convert")
-    var value = readLine()!!.toDouble()
+    try {
+        val measurementUnit = readLine()
+        val formattedMeasurementUnit = measurementUnit
+        println("Enter value to convert")
+        var value = readLine()!!.toDouble()
 
-    fun convertValues(baseUnit: String, baseValue: Double) {
-        var result: Double = value * baseValue
-        println("$value$formattedMeasurementUnit is $result in $baseUnit")
-    }
-
-    when (formattedMeasurementUnit?.toLowerCase()) {
-        "km" -> convertValues("Mi", kMTOMI)
-
-        "mi" -> convertValues("Km", mITOKM)
-
-        "cm" -> convertValues("Inch", cMTOINCH)
-
-        "inch" -> convertValues("Cm", iNCHTOCM)
-
-        "lib" -> convertValues("Kg", lBTOKG)
-
-        "kg" -> convertValues("Li", kGTOLB)
-
-        "ox" -> convertValues("G", oZTOG)
-
-        "gram" -> convertValues("Oz", gTOOZ)
-
-        else -> {
-            println("specified unit not found")
-            return
+        fun convertValues(baseUnit: String, baseValue: Double) {
+            var result: Double = value * baseValue
+            println("$value$formattedMeasurementUnit is $result in $baseUnit")
         }
+
+        when (formattedMeasurementUnit?.toLowerCase()) {
+            "km" -> convertValues("Mi", kMTOMI)
+
+            "mi" -> convertValues("Km", mITOKM)
+
+            "cm" -> convertValues("Inch", cMTOINCH)
+
+            "inch" -> convertValues("Cm", iNCHTOCM)
+
+            "lib" -> convertValues("Kg", lBTOKG)
+
+            "kg" -> convertValues("Li", kGTOLB)
+
+            "ox" -> convertValues("G", oZTOG)
+
+            "gram" -> convertValues("Oz", gTOOZ)
+
+            else -> {
+                println("specified unit not found")
+                return
+            }
+        }
+    } catch (e: NumberFormatException) {
+        println("You entered an invalid value")
     }
 }
